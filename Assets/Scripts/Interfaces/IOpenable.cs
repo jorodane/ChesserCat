8c22ac58518442620d3c0ed5ae48c4a711d0b149
jorodane <jorodane@naver.com>
2026-03-18 16:49:27 +0900

    Add Loading Screen

---
+++ Assets/Scripts/Interfaces/IOpenable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface IOpenable
+{
+	//ISP => Interface Segragation Principle => 인터페이스 분리 원칙
+	public bool IsOpen { get; }
+	public void Open();
+	public void Close();
+	public void Toggle();
+}
